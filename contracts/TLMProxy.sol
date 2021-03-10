020ec72e866f89e1387373048d67c6c840fe0b96
Yash Sinha <yash@Yashs-MBP.the-beach.net>
2021-03-10 12:43:49 -0500

    clean up comments and remove unused imports

--- contracts/TLMProxy.sol
+++ contracts/TLMProxy.sol
@@ -4,37 +4,29 @@ pragma solidity ^0.6.10;
 import "@openzeppelin/contracts/math/SafeMath.sol"; // TODO: Bring into @yield-protocol/utils
 import "@yield-protocol/utils/contracts/math/DecimalMath.sol"; // TODO: Make into library
 import "@yield-protocol/utils/contracts/utils/YieldAuth.sol";
-import "@yield-protocol/vault-v1/contracts/interfaces/IFYDai.sol";
 import "@yield-protocol/vault-v1/contracts/interfaces/IController.sol";
-import "@yield-protocol/yieldspace-v1/contracts/interfaces/IPool.sol";
-import "dss-interfaces/src/dss/AuthGemJoinAbstract.sol";
 import "./interfaces/DssTlmAbstract.sol";
 
 contract TLMProxy is DecimalMath {
     using SafeCast for uint256;
     using SafeMath for uint256;
     using RoundingMath for uint256;
-    using YieldAuth for IFYDai;
     using YieldAuth for IController;
-    using YieldAuth for IPool;
 
-    event BorrowedDai(address indexed user);
-    event RepaidDebtEarly(address indexed user);
-    event RepaidDebtMature(address indexed user);
+    //event BorrowedDai(address indexed user);
+    //not used for now but perhaps we will emit it later
 
-    DaiAbstract public immutable dai;
     IController public immutable controller;
     
-    bytes32 public constant WETH = "ETH-A";
+    //not needed
+    //bytes32 public constant WETH = "ETH-A";
 
     constructor(IController _controller, DssTlmAbstract tlm_) public {
         controller = _controller;
         tlm = tlm_;
         }
 
-    /// @dev Borrow fyDai from Controller, sell it immediately for Dai in Maker's TLM for a maximum fyDai debt.
-    /// Must have approved the operator with `controller.addDelegate(borrowProxy.address)` or with `borrowDaiForMaximumFYDaiWithSignature`.
-    /// Caller must have called `borrowDaiForMaximumFYDaiWithSignature` at least once before to set proxy approvals.
+    /// @dev Borrow fyDai from Controller, sell it immediately for Dai in Maker's TLM, and return the received Dai amount
     /// @param collateral Valid collateral type.
     /// @param maturity Maturity of an added series
     /// @param to Wallet to send the resulting Dai to.
@@ -49,13 +41,6 @@ contract TLMProxy is DecimalMath {
         returns (uint256)
     {
         controller.borrow(collateral, maturity, msg.sender, address(this), fyDaiToBorrow);
-
-        //not sure if this is called correctly but this is how it's done in this example
-        //https://github.com/yieldprotocol/dss-tlm#approvals
-        //sellGem(bytes32 ilk, address usr, uint256 gemAmt)
-
-        //TODO: return the amount of dai generated from the sale instead
         return tsm.sellGem(to, fyDaiToBorrow);
-
     }
 }
